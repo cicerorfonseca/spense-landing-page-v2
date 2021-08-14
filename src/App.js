f7abd5a69688ab7dff52ee3237f5f3daca086ef9
Cicero Raldi da Fonseca <cfonseca@northern.co>
2021-08-14 11:26:30 -0400

    Add header markup and styles for mobile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import './css/normalize.css';
 import './css/styles.scss';
+import Header from './components/Header';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
