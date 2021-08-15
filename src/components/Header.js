6f6dbb4215a16ceb6ea9d866243bef026fcb2f6b
Cicero Raldi da Fonseca <cfonseca@northern.co>
2021-08-15 12:48:17 -0400

    Start desktop styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,33 +10,36 @@ const Header = () => {
   };
 
   return (
-    <header className={`container ${displayMobileMenu ? 'mobile-active' : ''}`}>
-      <div className='logo'>spence.</div>
-      <div className='menu-wrapper'>
-        <button onClick={toggleMobileMenu} className='mobile-menu-close'>
-          X
-        </button>
-        <nav>
-          <ul>
-            <li>
-              <a href='/'>Business</a>
-            </li>
-            <li>
-              <a href='/'>Pricing</a>
-            </li>
-            <li>
-              <a href='/'>Features</a>
-            </li>
-          </ul>
-        </nav>
-        <div className='account-links'>
-          <Button text='Login' />
-          <Button text='Get Started' />
+    <header>
+      <div className={`header-content container ${displayMobileMenu ? 'mobile-active' : ''}`}>
+        <div className='logo mobile'>spence.</div>
+        <div className='menu-wrapper'>
+          <div className='logo'>spence.</div>
+          <button onClick={toggleMobileMenu} className='mobile-menu-close'>
+            X
+          </button>
+          <nav>
+            <ul>
+              <li>
+                <a href='/'>Business</a>
+              </li>
+              <li>
+                <a href='/'>Pricing</a>
+              </li>
+              <li>
+                <a href='/'>Features</a>
+              </li>
+            </ul>
+          </nav>
+          <div className='account-links'>
+            <Button text='Login' />
+            <Button text='Get Started' />
+          </div>
         </div>
+        <button onClick={toggleMobileMenu} className='mobile-menu'>
+          <img src={mobilemenu} alt='mobile menu' />
+        </button>
       </div>
-      <button onClick={toggleMobileMenu} className='mobile-menu'>
-        <img src={mobilemenu} alt='mobile menu' />
-      </button>
     </header>
   );
 };
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -7,34 +7,38 @@ import Button from './Button';
 
 const HeroBanner = () => {
   return (
-    <section className='hero-banner container'>
-      <picture className='hero-image'>
-        <source srcSet={desktopimg} media='(min-width: 1200px)' />
-        <source srcSet={tabletimg} media='(min-width: 768px)' />
-        <img src={mobileimg} alt='April earning withdraw' />
-      </picture>
-      <h1>Share your unfiltered thoughts. Get paid.</h1>
-      <p>
-        Spence is an open platform for individuals to share their unfiltered thoughts. Discus the topics you love, and get paid for doing{' '}
-        <em>just</em> that.
-      </p>
-      <ul>
-        <li>
-          <img src={checkmark} alt='checkmark' />
-          Receive 99% of the earnings.
-        </li>
-        <li>
-          <img src={checkmark} alt='checkmark' />
-          Get paid via Debit Card, ACH, or PayPal.
-        </li>
-        <li>
-          <img src={checkmark} alt='checkmark' />
-          Withdraw your earnings anytime.
-        </li>
-      </ul>
-      <div className='form-container'>
-        <input type='text' id='email' placeholder='john@example.com' />
-        <Button buttonType='primary' text='Get Started' />
+    <section className='hero-banner'>
+      <div className='hero-banner-content container'>
+        <picture className='hero-image'>
+          <source srcSet={desktopimg} media='(min-width: 1024px)' />
+          <source srcSet={tabletimg} media='(min-width: 768px)' />
+          <img src={mobileimg} alt='April earning withdraw' />
+        </picture>
+        <div className='hero-details'>
+          <h1>Share your unfiltered thoughts. Get paid.</h1>
+          <p>
+            Spence is an open platform for individuals to share their unfiltered thoughts. Discus the topics you love, and get paid for doing{' '}
+            <em>just</em> that.
+          </p>
+          <ul>
+            <li>
+              <img src={checkmark} alt='checkmark' />
+              Receive 99% of the earnings.
+            </li>
+            <li>
+              <img src={checkmark} alt='checkmark' />
+              Get paid via Debit Card, ACH, or PayPal.
+            </li>
+            <li>
+              <img src={checkmark} alt='checkmark' />
+              Withdraw your earnings anytime.
+            </li>
+          </ul>
+          <div className='form-container'>
+            <input type='text' id='email' placeholder='john@example.com' />
+            <Button buttonType='primary' text='Get Started' />
+          </div>
+        </div>
       </div>
     </section>
   );
