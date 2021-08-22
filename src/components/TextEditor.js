e0f7855270d3b2c12cc1707920834b0b985e4243
Cicero Raldi da Fonseca <cfonseca@northern.co>
2021-08-22 15:47:55 -0400

    Remove unnecesary files

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -4,13 +4,15 @@ const TextEditor = () => {
   return (
     <section className='text-editor container'>
       <div className='content-wrapper'>
-        <h2 className='section-header'>A text editor like no other.</h2>
-        <p>Our text editor pulls you into focus mode with it's simplistic design and usability so you can put out your best writings.</p>
-        <p>
-          <a href='/' className='content-anchor'>
-            Text Editor Live Demo <i className='fas fa-long-arrow-alt-right'></i>
-          </a>
-        </p>
+        <div className='text-wrapper'>
+          <h2 className='section-header'>A text editor like no other.</h2>
+          <p>Our text editor pulls you into focus mode with it's simplistic design and usability so you can put out your best writings.</p>
+          <p>
+            <a href='/' className='content-anchor'>
+              Text Editor Live Demo <i className='fas fa-long-arrow-alt-right'></i>
+            </a>
+          </p>
+        </div>
         <div className='img-wrapper'>
           <img src={texteditor} alt='April earning withdraw' />
         </div>
